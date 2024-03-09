dece4d1e1648612ccff97c59aa72e51f28d20984
Suha <mansuh09@gmail.com>
2024-03-09 15:21:33 -0800

    implemented login sign up- making changes

--- fe/src/App.js
+++ fe/src/App.js
@@ -5,19 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-header-buttons">
+          <button>Login</button>
+          <button>Register</button>
+        </div>
       </header>
+      <div className="App-body">
+        {/* Calendar view and task panel will go here */}
+      </div>
     </div>
   );
 }
